331049f75b0828c5bb88ed7b3b94103fa69fa613
Hannes Sachsenhofer <hannes.sachsenhofer@epunkt.net>
2013-09-25 09:26:34 +0200

    Added button to view a note "stand-alone".

--- EverMark/Controllers/EditorController.cs
+++ EverMark/Controllers/EditorController.cs
@@ -10,16 +10,42 @@ namespace EvernoteMvcExample.Controllers
 
         public ActionResult Index(string note)
         {
-            var viewModel = new IndexViewModel();
+            return View(BuildViewModel(note));
+        }
+
+        public ActionResult Preview(string note)
+        {
+            return View(BuildViewModel(note));
+        }
+
+        [HttpPost]
+        public ActionResult Index(string note, string content)
+        {
             var noteStore = Evernote.GetNoteStore(User.Identity.Name);
 
             var n = noteStore.getNote(User.Identity.Name, note, true, false, false, false);
-            var notebook = noteStore.getNotebook(User.Identity.Name, n.NotebookGuid);
-            viewModel.Title = n.Title;
-            viewModel.NotebookAndTitle = notebook.Name + "\\" + n.Title;
-            viewModel.NoteId = note;
 
-            var xmlContent = XDocument.Parse(n.Content);
+            content = content.Replace("\r", "").Replace("\n", "<br />");
+
+            n.Content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\"><en-note><pre>" + (content ?? "").Trim() + "</pre></en-note>";
+            noteStore.updateNote(User.Identity.Name, n);
+
+            return RedirectToAction("Index", new { note });
+        }
+
+
+        private IndexViewModel BuildViewModel(string noteId)
+        {
+            var viewModel = new IndexViewModel();
+            var noteStore = Evernote.GetNoteStore(User.Identity.Name);
+
+            var note = noteStore.getNote(User.Identity.Name, noteId, true, false, false, false);
+            var notebook = noteStore.getNotebook(User.Identity.Name, note.NotebookGuid);
+            viewModel.Title = note.Title;
+            viewModel.NotebookAndTitle = notebook.Name + "\\" + note.Title;
+            viewModel.NoteId = noteId;
+
+            var xmlContent = XDocument.Parse(note.Content);
             if (xmlContent.Root != null)
             {
                 var content = xmlContent.Root.ToString();
@@ -37,22 +63,8 @@ namespace EvernoteMvcExample.Controllers
                 viewModel.Content = content.Trim();
             }
 
-            return View(viewModel);
+            return viewModel;
         }
 
-        [HttpPost]
-        public ActionResult Index(string note, string content)
-        {
-            var noteStore = Evernote.GetNoteStore(User.Identity.Name);
-
-            var n = noteStore.getNote(User.Identity.Name, note, true, false, false, false);
-
-            content = content.Replace("\r", "").Replace("\n", "<br />");
-
-            n.Content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\"><en-note><pre>" + (content ?? "").Trim() + "</pre></en-note>";
-            noteStore.updateNote(User.Identity.Name, n);
-
-            return RedirectToAction("Index", new { note });
-        }
     }
 }
